217bd9febb7160e3a4652013ed32eef70810d2da
Allen Wild <allenwild93@gmail.com>
2023-04-01 21:13:00 -0400

    support background clipboard waiting, add clap CLI args
    Apparently something in Gnome immediately grabs things from the
    clipboard so we don't have to wait, but a bare X11 server like with WSL2
    (and possibly SSH forwarding, though I haven't tested that) we do
    actually need to stay alive to service clipboard requests.
    
    arboard's "daemonize" example re-execs itself with a special argument,
    but fw is simple enough that we can just use a single fork() safely.
    
    Also bring in clap and add some options to control whether and how we
    save to the clipboard.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-use std::io::{self, Read};
+use std::io::Read;
 
 use anyhow::Context;
+use arboard::{Clipboard, SetExtLinux};
+use clap::{Arg, ArgAction, ArgGroup};
 
 const WIDE_SPACE: char = '\u{3000}';
 const FULLWIDTH_OFFSET: u32 = 0xFEE0;
@@ -13,19 +15,133 @@ fn fw_char(c: char) -> char {
     }
 }
 
+#[derive(Debug, Clone, Copy)]
+enum WaitMode {
+    NoWait,
+    Foreground,
+    Background,
+}
+
+fn set_clipboard(text: &str, wait: WaitMode) -> anyhow::Result<()> {
+    /// Inner function to do *all* of the clipboard stuff, but without any fork shennanigans. This
+    /// may run in the main parent or child process.
+    fn inner(text: &str, wait: bool) -> anyhow::Result<()> {
+        let mut cb = Clipboard::new().context("failed to init clipboard")?;
+        let mut set = cb.set();
+        if wait {
+            set = set.wait();
+        }
+        set.text(text).context("failed to set clipboard contents")
+    }
+
+    match wait {
+        WaitMode::NoWait => inner(text, false),
+        WaitMode::Foreground => inner(text, true),
+        WaitMode::Background => {
+            // Fork to the background, then set the clipboard and wait in the background process.
+            // The parent will return Ok immediately unless fork failed.
+            //
+            // This is just a single fork and then disown, we don't do setsid() and double-fork
+            // like a "proper" daemon, because it doesn't seem necessary. We also keep stdio open
+            // so we can print errors if needed.
+            //
+            // SAFETY: "After a fork() in a multithreaded program, the child can safely call only
+            // async-signal-safe functions until it calls execve(2)". This translates to: we MUST
+            // fork only when the process is single-threaded. Specifically, we MUST NOT initialize
+            // or touch any of the clipboard handling in the parent and then use it from the child,
+            // because arboard spawns a helper thread for X11 clipboard handling. As long as we're
+            // single threaded at this point, the fork is safe, and all subsequent threads are only
+            // used in the context of the child process.
+            match unsafe { libc::fork() } {
+                // fork failed
+                -1 => Err(std::io::Error::last_os_error()).context("fork failed"),
+
+                // child process, set the clipboard and exit.
+                0 => {
+                    let retcode = match inner(text, true) {
+                        Ok(()) => 0,
+                        Err(err) => {
+                            eprintln!("fw clipboard error: {err:#}");
+                            1
+                        }
+                    };
+                    std::process::exit(retcode);
+                }
+
+                // parent process, return success immediately, implicitly disown the child.
+                _child_pid => Ok(()),
+            }
+        }
+    }
+}
+
+fn env_is_nonempty(var: &str) -> bool {
+    match std::env::var_os(var) {
+        Some(val) => !val.is_empty(),
+        None => false,
+    }
+}
+
 fn run() -> anyhow::Result<()> {
+    let args = clap::command!()
+        .about("Convert text to fullwidth glyphs (for cate memes)")
+        .arg(
+            Arg::new("no-clipboard")
+                .short('n')
+                .long("no-clipboard")
+                .action(ArgAction::SetTrue)
+                .help("Don't copy the output to the system clipboard"),
+        )
+        .arg(
+            Arg::new("no-wait")
+                .short('W')
+                .long("no-wait")
+                .action(ArgAction::SetTrue)
+                .help(
+                    "Don't wait for the clipboard to be reset before exiting. (This is the \
+                       default if a desktop session is detected)",
+                ),
+        )
+        .arg(
+            Arg::new("foreground-wait")
+                .short('F')
+                .long("foreground-wait")
+                .action(ArgAction::SetTrue)
+                .help(
+                    "Wait for the clipboard to be reset in the foreground rather than \
+                       forking to the background",
+                ),
+        )
+        .arg(
+            Arg::new("text")
+                .action(ArgAction::Append)
+                .required(false)
+                .help(
+                    "Text strings to convert. Arguments will be joined with spaces. \
+                      Omit to read stdin instead.",
+                ),
+        )
+        .group(
+            // our clipboard arguments are multually-exclusive
+            ArgGroup::new("clipboard-args")
+                .args(["no-clipboard", "no-wait", "foreground-wait"])
+                .required(false)
+                .multiple(false),
+        )
+        .get_matches();
+
     let mut text = String::new();
-    let mut args = std::env::args().skip(1).peekable();
-    if args.peek().is_some() {
-        while let Some(arg) = args.next() {
-            text.extend(arg.chars().map(fw_char));
-            if args.peek().is_some() {
+    if args.contains_id("text") {
+        let mut words = args.get_many::<String>("text").unwrap().peekable();
+        while let Some(word) = words.next() {
+            text.extend(word.chars().map(fw_char));
+            if words.peek().is_some() {
                 text.push(WIDE_SPACE);
             }
         }
     } else {
         let mut input = String::new();
-        io::stdin()
+        std::io::stdin()
             .lock()
             .read_to_string(&mut input)
             .context("failed to read stdin")?;
@@ -37,10 +153,24 @@ fn run() -> anyhow::Result<()> {
     }
     println!("{text}");
 
-    arboard::Clipboard::new()
-        .context("failed to init clipboard")?
-        .set_text(&text)
-        .context("failed to set clipboard contents")?;
+    if !args.get_flag("no-clipboard") && env_is_nonempty("DISPLAY") {
+        let mode = if args.get_flag("no-wait") {
+            WaitMode::NoWait
+        } else if args.get_flag("foreground-wait") {
+            WaitMode::Foreground
+        } else if env_is_nonempty("XDG_CURRENT_DESKTOP") {
+            // In Gnome, it seems like we can get away with setting the clipboard then immediately
+            // exiting. I guess something else in the desktop session picks it up.
+            // TODO verify that this is the right env var to check
+            WaitMode::NoWait
+        } else {
+            // by default if we don't think we're in a desktop session, fork to the background to
+            // wait and serve clipboard requests.
+            WaitMode::Background
+        };
+
+        set_clipboard(&text, mode)?;
+    }
 
     Ok(())
 }
